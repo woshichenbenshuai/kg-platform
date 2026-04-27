6e28e22fde2d7b307eb61d46eea5bf133d3692d6
sn_chenbenshuai <15106746368@163.com>
2026-04-27 15:56:05 +0800

    完善逻辑

--- kg-system/src/main/java/com/kgplatform/system/mapper/DictTypeMapper.java
+++ kg-system/src/main/java/com/kgplatform/system/mapper/DictTypeMapper.java
@@ -1,36 +1,37 @@
-package com.kgplatform.system.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.kgplatform.system.domain.dto.DictTypeDto;
-import com.kgplatform.system.domain.po.DictType;
-import com.kgplatform.system.domain.vo.DictTypeVo;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Component;
-
-/**
- * 系统字典类型
- * <p>
+package com.kgplatform.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.kgplatform.system.domain.dto.DictTypeDto;
+import com.kgplatform.system.domain.po.DictType;
+import com.kgplatform.system.domain.vo.DictTypeVo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * 系统字典类型
+ * <p>
  * DictType表数据库访问
- *
- * @author kg_chen
- * @since 2023-12-29 11:05:49
- */
-@Mapper
-@Component
-public interface DictTypeMapper extends BaseMapper<DictType> {
-
-
-    /**
-     * 查询列表
-     *
-     * @param page 分页
-     * @param vo   入参
-     * @return 结果
-     */
-    Page<DictTypeDto> selectPageList(Page<DictTypeDto> page,
-                                     @Param("vo") DictTypeVo vo);
-}
-
+ *
+ * @author kg_chen
+ * @since 2023-12-29 11:05:49
+ */
+@Mapper
+@Component
+public interface DictTypeMapper extends BaseMapper<DictType> {
+
+    /**
+     * 查询列表
+     *
+     * @param page 分页
+     * @param vo   入参
+     * @return 结果
+     */
+    Page<DictTypeDto> selectPageList(Page<DictTypeDto> page,
+                                     @Param("vo") DictTypeVo vo);
+}
+
+
+
--- kg-system/src/main/java/com/kgplatform/system/mapper/DictValueMapper.java
+++ kg-system/src/main/java/com/kgplatform/system/mapper/DictValueMapper.java
@@ -1,52 +1,55 @@
-package com.kgplatform.system.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.kgplatform.system.domain.dto.DictValueDto;
-import com.kgplatform.system.domain.po.DictValue;
-import com.kgplatform.system.domain.vo.DictValueVo;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Component;
-
-/**
- * 系统字典数据
- * <p>
+package com.kgplatform.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.kgplatform.system.domain.dto.DictValueDto;
+import com.kgplatform.system.domain.po.DictValue;
+import com.kgplatform.system.domain.vo.DictValueVo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+/**
+ * 系统字典数据
+ * <p>
  * DictValue表数据库访问
- *
- * @author kg_chen
- * @since 2023-12-29 11:07:02
- */
-@Mapper
-@Component
-public interface DictValueMapper extends BaseMapper<DictValue> {
-
-    /**
-     * 查询列表
-     *
-     * @param page 分页
-     * @param vo   入参
-     * @return 结果
-     */
-    Page<DictValueDto> selectPageList(Page<DictValueDto> page,
-                                      @Param("vo") DictValueVo vo);
-
-    /**
-     * 根据字典类型更新子字典的类型编码
-     *
-     * @param id   id
-     * @param code 类型
-     */
-    void updateDictTypeCode(@Param("id") Long id, @Param("code") String code);
-
-    /**
+ *
+ * @author kg_chen
+ * @since 2023-12-29 11:07:02
+ */
+@Mapper
+@Component
+public interface DictValueMapper extends BaseMapper<DictValue> {
+
+    /**
+     * 查询列表
+     *
+     * @param page 分页
+     * @param vo   入参
+     * @return 结果
+     */
+    Page<DictValueDto> selectPageList(Page<DictValueDto> page,
+                                      @Param("vo") DictValueVo vo);
+
+
+    /**
+     * 根据字典类型更新子字典的类型编码
+     *
+     * @param id   id
+     * @param code 类型
+     */
+    void updateDictTypeCode(@Param("id") Long id, @Param("code") String code);
+
+    /**
      * 根据字典类型标记删除子字
-     *
-     * @param id id
-     */
-    void deleteByDictTypeId(@Param("id") Long id);
-
-}
-
-
+     *
+     * @param id id
+     */
+    void deleteByDictTypeId(@Param("id") Long id);
+
+}
+
+
+
+
+
